b87dab4b98d3ae8e4c48001559439f71face342e
Navyajalla <162877943+Navyajalla@users.noreply.github.com>
2025-06-27 12:47:58 +0530

    Add files via upload

---
+++ Logging Middleware/Logger.js
@@ -0,0 +1,6 @@
+// src/components/Logger.js
+export const logger = (event, details = {}) => {
+  const logs = JSON.parse(localStorage.getItem("appLogs")) || [];
+  logs.push({ timestamp: new Date().toISOString(), event, ...details });
+  localStorage.setItem("appLogs", JSON.stringify(logs));
+};
